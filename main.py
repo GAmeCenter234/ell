3dfb18f6809ccedfdccfa5fa1a6c1607b632797c
Navi_Simple <mazuninvasilii@gmail.com>
2021-10-24 15:35:50 +0500

    master

---
+++ main.py
@@ -0,0 +1,19 @@
+from time import strftime
+import eel
+import pyowm
+
+city= "Perm, Russia"
+
+owm = pyowm.OWM("57ce3cdbaaba85c3ff9e217e6b0c39b5")
+mgr = owm.weather_manager()
+
+observation = mgr.weather_at_place(city)
+w = observation.weather
+
+temp = w.temperature('celsius')['temp']
+
+print("В городе " + city + " сейчас " + str(temp) + " градусов!")
+
+
+#eel.init("web")
+#eel.start("main.html", size=("700, 700"))
